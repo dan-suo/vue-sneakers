603b2a6a7e4716748b3251d7c69debca7e16302b
dan-suo <maksaoad@gmail.com>
2024-10-08 14:23:58 +0300

    Start creating header

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,37 @@
 </script>
 
 <template>
-  <h1>DS blogging</h1>
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-10">
+
+    <header>
+
+      <div>
+        <img src="/logo.png" alt="Logo" />
+      <div>
+        <h2>Vue Sneakers</h2>
+        <p>Магазин топовых кроссовок</p>
+      </div>
+      </div>
+
+      <ul>
+        <li>
+          <img src="/cart.svg" alt="Cart" />
+          <b>45$</b>
+        </li>
+        <li>
+          <img src="/heart.svg" alt="Heart" />
+          <b>Закладки</b>
+        </li>
+        <li>
+          <img src="/profile.svg" alt="Profile icon" />
+          <b>Проофиль</b>
+        </li>
+      </ul>
+    </header>
+
+    <h1>DS blogging</h1>
+
+  </div>
 </template>
 
 <style scoped>
